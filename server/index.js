16957c2d5459e38ec54b9f0efe7c85abd7886512
Alexander Aldrick <alex@aldrick.dev>
2021-12-16 14:10:26 +0100

    Completed Challenge 1

--- server/index.js
+++ server/index.js
@@ -1,18 +1,49 @@
+// imports
+const SHA256 = require('crypto-js/sha256');
 const express = require('express');
+const EC = require('elliptic').ec; // elliptic curve cryptography library for the generation of public and private keys
 const app = express();
 const cors = require('cors');
 const port = 3042;
+
+// code copied from https://github.com/ChainShot/ECDSA-Signatures/blob/master/generate.js
+
+var ec = new EC('secp256k1'); // documentation says better to do once and reuse it.
+// "create EC conhash1"
+const key1 = ec.genKeyPair();
+const key2 = ec.genKeyPair();
+const key3 = ec.genKeyPair();
+
+const publicKey1 = key1.getPublic().encode('hex');
+const publicKey2 = key2.getPublic().encode('hex');
+const publicKey3 = key3.getPublic().encode('hex');
+
+const hash1 = SHA256(publicKey1).toString();
+const hash2 = SHA256(publicKey2).toString();
+const hash3 = SHA256(publicKey3).toString();
+
+// eth style address with truncation of the last 40 characters
+
+let address1 = hash1.substring(hash1.length - 40);
+let address2 = hash2.substring(hash2.length - 40);
+let address3 = hash3.substring(hash3.length - 40);
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
+// we want to use the https://github.com/paulmillr/noble-secp256k1 library to create public/private key pairs and then fund the addresses below
+
+// let's first try with https://www.npmjs.com/package/elliptic as it seems much more straightforward
+
 const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+  [address1]: 100,
+  [address2]: 50,
+  [address3]: 75,
+};
+
+console.log(balances); // as required by challenge 1
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
